6a1cc9312a4821dcabfde31bd733e9aea05c66ef
Marcos Henrique Ribeiro de Santana. <161729448+MarcosHenriqueRS10@users.noreply.github.com>
2025-04-15 10:21:39 -0300

    Add files via upload

---
+++ menu_item.dart
@@ -0,0 +1,70 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:pizza_menu/pizza_data.dart';
+
+class MenuItem extends StatelessWidget {
+  final Pizza pizza;
+  const MenuItem({super.key, required this.pizza});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Row(
+          children: [
+            ColorFiltered(
+              colorFilter: pizza.soldOut?
+             ColorFilter.mode(Colors.grey, BlendMode.saturation) :
+             ColorFilter.mode(Colors.transparent, BlendMode.multiply),
+              child: Image.asset(
+                "images/${pizza.photoName}",
+                width: 100,
+                height: 100,
+              ),
+            ),
+            SizedBox(
+              width: 16,
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    pizza.name,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Text(
+                   pizza.ingredients,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Text(
+                    pizza.soldOut ? "sold Out" : "\$${pizza.price}",
+                    style: TextStyle(
+                      color: Colors.black45,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ pizza_menu.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:pizza_menu/menu_item.dart';
+import 'package:pizza_menu/pizza_data.dart';
+
+class PizzaMenu extends StatelessWidget {
+  const PizzaMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pizza Menu'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                 ...pizzaData.map((p) => MenuItem(pizza: p,))
+                ],
+              ),
+            ),
+          ),
+          if(false)
+           Padding(
+             padding: const EdgeInsets.all(12.0),
+             child: SizedBox(
+              width: double.infinity,
+               child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.yellow[600]
+                ),
+                onPressed: () {},
+                child: Text("Order now!")
+                 ),
+             ),
+           )
+        ],
+      ),
+    );
+  }
+
+  List<MenuItem> _buildPizzas() {
+    List<MenuItem> pizzas = [];
+    for (var p in pizzaData){
+      pizzas.add(MenuItem(pizza: p));
+    }
+    return pizzas;
+  }
+}
